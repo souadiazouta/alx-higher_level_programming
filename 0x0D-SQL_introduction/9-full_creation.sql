7cd08296204db6dea68f70909290b025cfdd910e
souadiazzouta <souad.IAZZOUTA@gmail.com>
2022-08-16 16:44:47 -0700

    Full creation

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS second_table (
 	score INT
 	);
 INSERT INTO second_table VALUES 
-(1, 'john', 10),
-(2, 'Alex', 3),
-(3, 'Bob', 14),
-(4, 'George', 8);
+(1, "John", 10),
+(2, "Alex", 3),
+(3, "Bob", 14),
+(4, "George", 8);
