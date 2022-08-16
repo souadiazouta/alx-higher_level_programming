2bec553bf5156fc4026fcbdf35fbe1bdc9c5d43a
souadiazzouta <souad.IAZZOUTA@gmail.com>
2022-08-16 16:42:30 -0700

    Full creation

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
 -- creates a table second_table in the database hbtn_0c_0
 -- and create four records
-CREATE TABLES IF NOT EXISTS second_table (
+CREATE TABLE IF NOT EXISTS second_table (
 	id INT,
 	name VARCHAR(256),
 	score INT
