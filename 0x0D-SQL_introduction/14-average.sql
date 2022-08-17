39b574d222b6bd588641a6271eabf60beda0c75e
souadiazzouta <souad.IAZZOUTA@gmail.com>
2022-08-17 05:21:47 -0700

    number by score

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,4 @@
+-- Write a script that computes the score average of all records in the table second_table of the database hbtn_0c_0
+-- the result column name should be average
+SELECT AVG(score) as average
+FROM second_table;
