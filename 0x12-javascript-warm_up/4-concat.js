99bf4b179b8feb1e01b4fb72db8080c725b3a067
souadiazzouta <souad.IAZZOUTA@gmail.com>
2022-09-07 18:55:47 -0700

    create a sentence

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -2,4 +2,4 @@
 // prints two argumnets passed to it,
 // in the following format: "is"
 
-console.log(process.argv[2]+' is '+process.argv[3]);
+console.log(process.argv[2] + ' is ' + process.argv[3]);
