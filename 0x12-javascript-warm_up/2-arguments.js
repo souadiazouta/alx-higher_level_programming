184d00c3ecb8206f0f8dc7d0c6531bf12cb86059
souadiazzouta <souad.IAZZOUTA@gmail.com>
2022-09-07 18:35:47 -0700

    process.argv

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 // print a message depending of the number of argument passed
 
-if (process.argv.length === 2){
+if (process.argv.length === 2) {
 	console.log("No argument");
-} else if (process.argv.length === 3){
+} else if (process.argv.length === 3) {
 	console.log("Argument found");
 } else {
 	console.log("Arguments found");
