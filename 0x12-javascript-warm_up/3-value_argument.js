4f84a299155ea3f175565792fa52a736954b4ad9
souadiazzouta <souad.IAZZOUTA@gmail.com>
2022-09-08 05:30:31 -0700

    print first argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,7 +2,7 @@
 // prints the first argumnets passed to it
 
 if (process.argv[2] === undefined) {
-	console.log('No argument');
+  console.log('No argument');
 } else {
-	console.log(process.argv[2]);
+  console.log(process.argv[2]);
 }
