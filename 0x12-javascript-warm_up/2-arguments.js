fc2d02959975ac127c04b7a63939546a672548bc
souadiazzouta <souad.IAZZOUTA@gmail.com>
2022-09-08 05:31:30 -0700

    print first argument

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,10 +2,10 @@
 // print a message depending of the number of argument passed
 
 if (process.argv.length === 2) {
-	console.log('No argument');
+  console.log('No argument');
 } else if (process.argv.length === 3) {
-	console.log('Argument found');
+  console.log('Argument found');
 } else {
-	console.log('Arguments found');
+  console.log('Arguments found');
 }
 
