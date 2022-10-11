3b3cfe82867a5e197bf4248988714d757f159737
souadiazzouta <souad.IAZZOUTA@gmail.com>
2022-10-11 08:24:19 -0700

    print 3 lines

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 // print 3 lines: (like 1-multi_languages) but by
 // using an array of string and a loop
-//
+
 const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
 for (let i = 0; i < array.length; i++) {
 console.log(array[i]);
 }
