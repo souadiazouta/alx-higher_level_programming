f4e8cd7cff7887883e5a76be7c3a3cab9c158fdc
souadiazzouta <souad.IAZZOUTA@gmail.com>
2022-10-11 08:06:13 -0700

    print 3 lines

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // print 3 lines: but by using an array of string and a loop
-const array = ["C is fun", "Python is cool", "Javascript is amazing"];
+const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 for (let i = 0; i < array.length; i++) {
 console.log(array[i]);
 }
