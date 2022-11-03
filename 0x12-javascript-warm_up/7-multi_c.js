ced6fd20b341b2f815753890026e2e35aa83c44e
souadiazzouta <souad.iazzouta@gmail.com>
2022-11-03 05:32:20 -0700

    javascript warm up

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -5,7 +5,7 @@ const print = 'C is fun';
 if (isNaN(process.argv[2])) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i <parseInt(process.argv[2]); i++) {
+  for (let i = 0; i < parseInt(process.argv[2]); i++) {
     console.log(print);
   }
 }
